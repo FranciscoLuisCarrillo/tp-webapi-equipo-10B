d4cb87f983b5dff3ceaf7ac9a4b3e94172275afc
Stepanuck <marinozzitomas@hotmail.com>
2025-10-13 20:56:12 -0300

    TM - 3 - ArticuloDTO
    Agregado de la clase ArticuloDTO en la carpeta Models

---
+++ webApi10B/Models/ArticuloDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webApi10B.Models
+{
+    public class ArticuloDTO
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
